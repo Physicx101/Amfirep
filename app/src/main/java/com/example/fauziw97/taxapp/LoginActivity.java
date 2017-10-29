625649cc39e7428f5fa3650d9186e2ac54f8a43f
tommywahyu44 <tommywahyu44@gmail.com>
2017-10-29 21:48:22 +0700

    Login Activity with shared preferences

---
+++ app/src/main/java/com/example/fauziw97/taxapp/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.example.fauziw97.taxapp;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+
+public class LoginActivity extends AppCompatActivity  {
+
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Auth = "authKey";
+    SharedPreferences sharedpreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        Button mMemberSignInButton = (Button) findViewById(R.id.email_sign_in);
+        mMemberSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+
+
+    }
+
+
+    private void attemptLogin() {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError("Email tidak boleh kosong");
+
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError("Email tidak valid");
+
+
+        }
+
+        if (email.equalsIgnoreCase("admin@amfirep.com") && password.equalsIgnoreCase("amfirep")){
+
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString(Auth, "Admin");
+            editor.commit();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+        } else if(email.equalsIgnoreCase("admin@amfirep.com")) {
+            mPasswordView.setError("Password Salah");
+
+        } else mEmailView.setError("Email Salah");
+
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+}
+
--- app/src/main/java/com/example/fauziw97/taxapp/SplashActivity.java
+++ app/src/main/java/com/example/fauziw97/taxapp/SplashActivity.java
@@ -1,9 +1,15 @@
 package com.example.fauziw97.taxapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.AutoScrollHelper;
 import android.support.v7.app.AppCompatActivity;
+
+import static com.example.fauziw97.taxapp.LoginActivity.Auth;
+import static com.example.fauziw97.taxapp.LoginActivity.MyPREFERENCES;
 
 /**
  * Created by Fauziw97 on 9/12/17.
@@ -11,12 +17,23 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    SharedPreferences sharedpreferences;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String Authen = sharedpreferences.getString(Auth,null);
+        if (Authen != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
